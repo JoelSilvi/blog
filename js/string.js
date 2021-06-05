7e251b781d5d0b66376e2535e6f65b9cd52831d0
JoelSilvi <analistasilvi@gmail.com>
2021-06-05 04:02:57 -0400

    Agregando Js

---
+++ js/string.js
@@ -0,0 +1,30 @@
+const nombres = [
+  "adolfo",
+  "juan",
+  "pedro",
+  "luis",
+  "camel",
+  "rodolfo",
+  "acata",
+  "cada",
+];
+
+// nombres.push("felipe");
+
+// nombres.unshift("calipso");
+
+// nombres.reverse();
+
+nombres.sort();
+
+// nombres.splice(0, 2);
+
+// nombres.splice(0, 0, "camilo", "perico", "castaño");
+
+// nombres[nombres.length] = "rosado";
+
+// const contador = `la cantidad de nombres es: ${nombres.length}`;
+
+console.log(nombres);
+
+// console.log(contador);
